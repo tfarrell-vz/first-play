ae424176a5d6068308697902fac2dfd4610fe530
tfarrell-vz <farrell@abiresearch.com>
2015-05-14 16:37:17 -0400

    Hello world! Also, square a number in your browser! Doesn't check type - be careful.

--- first-play/app/controllers/Application.scala
+++ first-play/app/controllers/Application.scala
@@ -6,7 +6,19 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok("Hello, World!")
+  }
+
+  def hello(name: String) = Action {
+    Ok("Hello " + name)
+  }
+
+  def squareIt(num: Int): Int = {
+    num * num
+  }
+
+  def square(num: String) = Action {
+    Ok(squareIt(num.toInt).toString)
   }
 
 }
